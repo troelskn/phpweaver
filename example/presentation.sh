f648f99373ebca943fdfa71891d96512a4618cdf
Anders Jenbo <anders@jenbo.dk>
2017-12-19 01:31:14 +0100

    Remove diff dependency from example script

--- example/presentation.sh
+++ example/presentation.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 ../bin/phpweaver trace controller.php
-cp Foo.php Foo.out.php
-../bin/phpweaver weave Foo.out.php --overwrite
-diff Foo.php Foo.out.php
+../bin/phpweaver weave ./Foo.php
+
